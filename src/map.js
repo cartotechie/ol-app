1f68d76843edd8d7437067a9c2742d621082d4a9
schawanji <schawanji@gmail.com>
2024-01-01 23:30:59 +0100

    Fixed the search form

--- src/map.js
+++ src/map.js
@@ -55,11 +55,11 @@ import {
 import {textPointStyle,customSVGPointStyle,starPointStyle,crossPointStyle,squarePointStyle,defaultPointStyle}from './modules/pointStyle'
 import {classesValues} from './modules/dataStore'
 import{createDropdownTable} from './modules/dropDownTable' 
-const geoJsonEndpoint = 'http://gdfworldbank/worldbank/geojsonapi/';
+const geoJsonEndpoint = './data/powertowers.json';
 
 
-
-let selectedProvince //= 'Sylhet Division';
+let searchTerm
+let selected //= 'Sylhet Division';
 let selectedDistrict //= 'Sylhet District';
 let valueUpazila
 let selectedDistricts = [];
@@ -67,7 +67,7 @@ let selectedUpezillas = [];
 
 const selectedPointStyle = (feature)=>{
   
-    if(feature.get(province)===selectedProvince||feature.get(commune)===selectedDistrict||feature.get('name_en')===valueUpazila){
+    if(feature.get(province)===selected||feature.get(commune)===selectedDistrict||feature.get('name_en')===valueUpazila){
      //cases= feature.get('cases')
     //deaths=  feature.get('deaths')
     console.log('selected')
@@ -79,7 +79,7 @@ return glowingStyle
 }
 const selectedPolyStyle = (feature)=>{
   
-    if(feature.get(province)===selectedProvince||feature.get(commune)===selectedDistrict||feature.get('name_en')===valueUpazila){
+    if(feature.get(province)===selected||feature.get(commune)===selectedDistrict||feature.get('name_en')===valueUpazila){
      //cases= feature.get('cases')
     //deaths=  feature.get('deaths')
     console.log('selected')
@@ -101,7 +101,6 @@ for (let i = 0; i < divName.length; i++) {
     divName[i].textContent;
 }
 
-const getSearchTerm=()=>{} 
 
 createDropdownTable(4, 4); 
 /****************************************************************************************** */
@@ -244,45 +243,64 @@ getLayers();
 
 
 
+
+
+
 /************************************************************************ */
 
+/*const getSearchTerm=(event)=>{
+    event.preventDefault
+    const searchInputTerm = document.getElementById('search-input')
+    console.log(searchInputTerm)} */
+
+    function getSearchTerm(event) {
+        event.preventDefault
+        
+        const searchInputTerm = document.getElementById('search-input')
+        
+        
+        const layer = map.getAllLayers()[2]
+        const features = layer.getSource().getFeatures();
+        
+        onclickDivision(searchInputTerm.value)
+        
+
+        
+    }
+//const searchFormElement = document.getElementById('geocode-form')
+document.getElementById('geocode-form').addEventListener('click',getSearchTerm)
 
 
 
-function onchangeDivision(event) {
+function onclickDivision(term) {
+
     const layer = map.getAllLayers()[2]
     const features = layer.getSource().getFeatures();
 
-    event.preventDefault();
-    selectedProvince = event.target.value;
-    adminName.innerHTML = `${selectedProvince}`
+    searchTerm = term;
+    adminName.innerHTML = `${searchTerm}`
 
 
 
-    const filteredFeatures = features.filter(feature => feature.get(province) === selectedProvince);
-    const extent = olExtent.boundingExtent(filteredFeatures.map(feature => feature.getGeometry().getExtent()));
-
+    const filteredFeatures = features.filter(feature => feature.get(province)=== searchTerm||feature.get(commune)=== searchTerm||feature.get(upezilla) === searchTerm);
+    if (filteredFeatures.length>0) {
+        const extent = olExtent.boundingExtent(filteredFeatures.map(feature => feature.getGeometry().getExtent()));
+    console.log(filteredFeatures)
     map.getView().fit(extent, {
         padding: [10, 10, 10, 10],
         duration: 1000
     });
+    } else {
+        console.log('No feature filtered')
+    }
+    
 
 
     
-    /* Apply the glowing style to the selected features
-filteredFeatures.forEach(feature => {
-    feature.setStyle(glowingStyle);
-});
+   
 
-// Optionally, remove the glowing effect after a certain time
-setTimeout(() => {
-    filteredFeatures.forEach(feature => {
-        feature.setStyle(null); // Remove the style to revert to the default
-    });
-}, 10000); // Adjust the time (in milliseconds) as needed*/
-
-    //console.log(filteredFeatures)
-    countDisplayedFeatures.innerHTML = `${filteredFeatures.length} Powerlines`
+    
+    /**countDisplayedFeatures.innerHTML = `${filteredFeatures.length} Powerlines`
     const uniqueCommunesCount = countFeatures(filteredFeatures, commune, selectedDistricts);
     updateCountDisplay(countDisplayDistricts, uniqueCommunesCount, 'Districts');
     updateCountDisplay(countDisplayDivisions, '', '')
@@ -296,7 +314,7 @@ setTimeout(() => {
     /*****************************************************************************/
 
 
-    featureKeys.forEach(className => className)
+    /**featureKeys.forEach(className => className)
     // Iterate over the array using forEach
 
     const filteredData = generateChartData(filteredFeatures);
@@ -306,10 +324,6 @@ setTimeout(() => {
     clearDistCharts()
     clearUpazilaCharts()
 
-    for (let i = 0; i < divName.length; i++) {
-
-        divName[i].textContent = selectedProvince
-    }
 
 
 
@@ -318,147 +332,9 @@ setTimeout(() => {
     createChart(`graph_div_class_1_13`, `bar`, filteredData.class_1_13);
     createChart(`graph_div_class_1_14`, `bar`, filteredData.class_1_14);
     createChart(`graph_div_class_1_15`, `bar`, filteredData.class_1_15);
-    createChart(`graph_div_class_12`, `bar`, filteredData.class_12);
+    createChart(`graph_div_class_12`, `bar`, filteredData.class_12);***/
 
     layer.changed();
 }
 
-document.getElementById("division").addEventListener('change', onchangeDivision);
 
-/****************************************************************************************** */
-
-function onchangeDistrict(event) {
-    const layer = map.getAllLayers()[2]
-    const features = layer.getSource().getFeatures();
-
-    //console.log(features)
-    event.preventDefault();
-    selectedDistrict = event.target.value;
-    adminName.innerHTML = `${selectedDistrict}`
-
-    const filteredFeatures = features.filter(feature => feature.get(commune) === selectedDistrict);
-    const extent = olExtent.boundingExtent(filteredFeatures.map(feature => feature.getGeometry().getExtent()));
-
-    map.getView().fit(extent, {
-        padding: [10, 10, 10, 10],
-        duration: 1000
-    });
-    countDisplayedFeatures.innerHTML = `${filteredFeatures.length} Powerlines`
-    //const uniqueCommunesCount = countFeatures(filteredFeatures, commune, selectedDistricts);
-    updateCountDisplay(countDisplayDistricts, '', '');
-    updateCountDisplay(countDisplayDivisions, '', '')
-
-    const uniqueUpezillasCount = countFeatures(filteredFeatures, 'name_en', selectedUpezillas);
-    updateCountDisplay(countDisplayUpezillas, uniqueUpezillasCount, 'Upezillas');
-    // Clear the existing table
-    clearTable();
-    generateTable(createUniqueAttributes(filteredFeatures, 'name_en'))
-    console.log(filteredFeatures)
-    const filteredData = generateChartData(filteredFeatures)
-
-    clearDistCharts()
-    clearUpazilaCharts()
-
-    for (let i = 0; i < distName.length; i++) {
-        distName[i].textContent = ''
-        distName[i].textContent = selectedDistrict; // You can perform actions on each element here
-    }
-
-
-    createChart(`graph_dist_class`, `bar`, filteredData.class);
-    createChart(`graph_dist_class_1`, `bar`, filteredData.class_1);
-    createChart(`graph_dist_class_1_13`, `bar`, filteredData.class_1_13);
-    createChart(`graph_dist_class_1_14`, `bar`, filteredData.class_1_14);
-    createChart(`graph_dist_class_1_15`, `bar`, filteredData.class_1_15);
-    createChart(`graph_dist_class_12`, `bar`, filteredData.class_12);
-
-    layer.changed();
-
-}
-
-document.getElementById('district').addEventListener('change', onchangeDistrict);
-
-/************************************************************************************************* */
-
-
-const displayUpazilaInfo = (event) => {
-    // Get the clicked row
-    const clickedRow = event.target.closest('tr');
-    
-    let valueDivision
-    let valueDistrict
-
-    // Check if a row was clicked
-    if (clickedRow) {
-        // Get data from the clicked row
-        const columns = clickedRow.querySelectorAll('td');
-        const rowData = Array.from(columns).map(column => column.textContent);
-
-        // Display the data in an alert
-        //alert('Clicked Row Data:\n' + rowData.join('\n'));
-        console.log(rowData[2])
-        valueDivision = rowData[0]
-        valueDistrict = rowData[1]
-        valueUpazila = rowData[2]
-
-    }
-
-    const layer = map.getAllLayers()[2]
-    const features = layer.getSource().getFeatures()
-    /********** generate table */
-
-    generateTable(createUniqueAttributes(features, 'name_en'))
-
-    /****************************+ */
-
-    const filterUpezillaFeatures = features.filter(feature => feature.get(upezilla) === valueUpazila)
-    const filterDistFeatures = features.filter(feature => feature.get(commune) === valueUpazila)
-    console.log(filterDistFeatures)
-    // Clear the existing table
-    clearUpazilaCharts()
-
-
-    // Get the extent of the filtered features
-    const extent = olExtent.boundingExtent(filterUpezillaFeatures.map(feature => feature.getGeometry().getExtent()));
-
-    map.getView().fit(extent, {
-        padding: [30, 30, 30, 30],
-        duration: 1000
-    });
-    // Sample data
-
-    // Update the content of the elements
-    document.getElementById('upezilla-name').innerHTML = `<strong>${valueUpazila}</strong>`;
-    document.getElementById('countUpezillaPowerlines').innerHTML = `${filterUpezillaFeatures.length} Powerlines `;
-
-    // Clear existing charts
-
-    for (let i = 0; i < upazilaName.length; i++) {
-        upazilaName[i].textContent = ''
-        upazilaName[i].textContent = valueUpazila
-    }
-
-
-
-
-    // Generate and create new charts
-    const filteredDataUpazila = generateChartData(filterUpezillaFeatures);
-
-    /*********** Upazila ********************************/
-    createChart(`graph_upazila_class`, `bar`, filteredDataUpazila.class);
-    createChart(`graph_upazila_class_1`, `bar`, filteredDataUpazila.class_1);
-    createChart(`graph_upazila_class_1_13`, `bar`, filteredDataUpazila.class_1_13);
-    createChart(`graph_upazila_class_1_14`, `bar`, filteredDataUpazila.class_1_14);
-    createChart(`graph_upazila_class_1_15`, `bar`, filteredDataUpazila.class_1_15);
-    createChart(`graph_upazila_class_12`, `bar`, filteredDataUpazila.class_12);
-
-}
-
-// Add click event listener to the table
-document.getElementById('table').addEventListener('click', displayUpazilaInfo);
-
-/********************************************* */
-
-map.on('click', function(event) {
-    handleMapInfoBoxClick(event, map)
-});
