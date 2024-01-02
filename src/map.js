f60442b4f892700a1186392b429c44e8b75726dc
schawanji <schawanji@gmail.com>
2024-01-02 13:18:30 +0100

    Fixed the charts

--- src/map.js
+++ src/map.js
@@ -262,7 +262,7 @@ getLayers();
         const layer = map.getAllLayers()[2]
         const features = layer.getSource().getFeatures();
         
-        onclickDivision(searchInputTerm.value)
+        onclickDivision(searchInputTerm.value.trim())
         
 
         
@@ -274,15 +274,22 @@ document.getElementById('geocode-form').addEventListener('click',getSearchTerm)
 
 function onclickDivision(term) {
 
+
     const layer = map.getAllLayers()[2]
     const features = layer.getSource().getFeatures();
 
     searchTerm = term;
     adminName.innerHTML = `${searchTerm}`
 
+    const divName = document.getElementsByClassName('div-name')
+    const distName = document.getElementsByClassName('dist-name')
+    const upazilaName = document.getElementsByClassName('upazila-name')
 
-
+    //const filteredFeatures = features.filter(feature => feature.get(province)=== searchTerm||feature.get(commune)=== searchTerm||feature.get(upezilla) === searchTerm);
     const filteredFeatures = features.filter(feature => feature.get(province)=== searchTerm||feature.get(commune)=== searchTerm||feature.get(upezilla) === searchTerm);
+    
+    
+    
     if (filteredFeatures.length>0) {
         const extent = olExtent.boundingExtent(filteredFeatures.map(feature => feature.getGeometry().getExtent()));
     console.log(filteredFeatures)
@@ -290,6 +297,174 @@ function onclickDivision(term) {
         padding: [10, 10, 10, 10],
         duration: 1000
     });
+        // Create charts
+        clearDivCharts()
+        clearDistCharts()
+        clearUpazilaCharts()
+
+        const filteredFeaturesDiv = features.filter(feature => feature.get(province)=== searchTerm);
+        const filteredFeaturesDist = features.filter(feature =>feature.get(commune)=== searchTerm);
+        const filteredFeaturesUpe = features.filter(feature => feature.get(upezilla) === searchTerm);
+    
+    
+    
+
+    if (filteredFeaturesDiv.length > 0){
+        for (let i = 0; i < divName.length; i++) {
+            divName[i].textContent = ''
+            divName[i].textContent = searchTerm; // You can perform actions on each element here
+        }
+        
+        for (let i = 0; i < distName.length; i++) {
+            distName[i].textContent = ''
+            
+              }
+        for (let i = 0; i < upazilaName.length; i++) {
+            upazilaName[i].textContent = ''
+            
+        }
+        let filteredDataDiv = generateChartData(filteredFeaturesDiv);
+        createChart(`graph_div_class`, `bar`, filteredDataDiv.class);
+        createChart(`graph_div_class_1`, `bar`, filteredDataDiv.class_1);
+        createChart(`graph_div_class_1_13`, `bar`, filteredDataDiv.class_1_13);
+        createChart(`graph_div_class_1_14`, `bar`, filteredDataDiv.class_1_14);
+        createChart(`graph_div_class_1_15`, `bar`, filteredDataDiv.class_1_15);
+        createChart(`graph_div_class_12`, `bar`, filteredDataDiv.class_12);
+
+    }
+     if (filteredFeaturesDist.length > 0){
+         
+
+        // Assuming you have already defined filteredFeaturesDist
+const uniqueValuesSetDiv = new Set();
+
+// Iterate through filteredFeaturesDist to get unique province values
+filteredFeaturesDist.forEach(feature => {
+    console.log(feature.get(province));
+    const attributeValue = feature.get(province);
+
+    // Check if the attributeValue is defined (not undefined)
+    if (attributeValue !== undefined) {
+        uniqueValuesSetDiv.add(attributeValue);
+    }
+});
+
+// Convert the Set to an array
+let uniquefilteredDataDiv = Array.from(uniqueValuesSetDiv);
+
+// Now, uniquefilteredDataDiv contains an array of unique province values
+console.log(uniquefilteredDataDiv);
+
+for (let i = 0; i < divName.length; i++) {
+    divName[i].textContent = ''
+    divName[i].textContent = uniquefilteredDataDiv[0]; // You can perform actions on each element here
+}
+
+for (let i = 0; i < distName.length; i++) {
+    distName[i].textContent = ''
+    distName[i].textContent = searchTerm; // You can perform actions on each element here
+}
+for (let i = 0; i < upazilaName.length; i++) {
+    upazilaName[i].textContent = ''
+    
+}
+
+        
+     let filteredData = features.filter(feature => feature.get(province)=== uniquefilteredDataDiv[0])
+
+        let filteredDataDiv = generateChartData(filteredData);
+        createChart(`graph_div_class`, `bar`, filteredDataDiv.class);
+        createChart(`graph_div_class_1`, `bar`, filteredDataDiv.class_1);
+        createChart(`graph_div_class_1_13`, `bar`, filteredDataDiv.class_1_13);
+        createChart(`graph_div_class_1_14`, `bar`, filteredDataDiv.class_1_14);
+        createChart(`graph_div_class_1_15`, `bar`, filteredDataDiv.class_1_15);
+        createChart(`graph_div_class_12`, `bar`, filteredDataDiv.class_12);
+
+        let filteredDataDist = generateChartData(filteredFeaturesDist)
+
+        createChart(`graph_dist_class`, `bar`, filteredDataDist.class);
+        createChart(`graph_dist_class_1`, `bar`, filteredDataDist.class_1);
+        createChart(`graph_dist_class_1_13`, `bar`, filteredDataDist.class_1_13);
+        createChart(`graph_dist_class_1_14`, `bar`, filteredDataDist.class_1_14);
+        createChart(`graph_dist_class_1_15`, `bar`, filteredDataDist.class_1_15);
+        createChart(`graph_dist_class_12`, `bar`, filteredDataDist.class_12);
+
+    }
+
+
+    if (filteredFeaturesUpe.length > 0){
+
+                // Assuming you have already defined filteredFeaturesDist
+const uniqueValuesSetDiv = new Set();
+
+// Iterate through filteredFeaturesDist to get unique province values
+filteredFeaturesUpe.forEach(feature => {
+    console.log(feature.get(province));
+    const attributeValue1 = feature.get(province);
+    const attributeValue2 = feature.get(commune);
+    const attributeValue3 = feature.get(upezilla);
+
+    // Check if the attributeValue1 is defined (not undefined)
+    if (attributeValue1 !== undefined &attributeValue2 !== undefined) {
+        uniqueValuesSetDiv.add(attributeValue1);
+        uniqueValuesSetDiv.add(attributeValue2);
+        uniqueValuesSetDiv.add(attributeValue3);
+    }
+});
+
+// Convert the Set to an array
+let uniquefilteredDataDiv = Array.from(uniqueValuesSetDiv);
+
+// Now, uniquefilteredDataDiv contains an array of unique province values
+console.log(uniquefilteredDataDiv);
+for (let i = 0; i < divName.length; i++) {
+    divName[i].textContent = ''
+    divName[i].textContent = uniquefilteredDataDiv[0]; // You can perform actions on each element here
+}
+
+for (let i = 0; i < distName.length; i++) {
+    distName[i].textContent = ''
+    distName[i].textContent = uniquefilteredDataDiv[1]; // You can perform actions on each element here
+}
+     let filteredDataDivUn = features.filter(feature => feature.get(province)=== uniquefilteredDataDiv[0])
+
+        let filteredDataDiv = generateChartData(filteredDataDivUn);
+        createChart(`graph_div_class`, `bar`, filteredDataDiv.class);
+        createChart(`graph_div_class_1`, `bar`, filteredDataDiv.class_1);
+        createChart(`graph_div_class_1_13`, `bar`, filteredDataDiv.class_1_13);
+        createChart(`graph_div_class_1_14`, `bar`, filteredDataDiv.class_1_14);
+        createChart(`graph_div_class_1_15`, `bar`, filteredDataDiv.class_1_15);
+        createChart(`graph_div_class_12`, `bar`, filteredDataDiv.class_12);
+       
+        let filteredDataComUn = features.filter(feature => feature.get(commune)=== uniquefilteredDataDiv[1])
+
+
+        let filteredDataDist = generateChartData(filteredDataComUn)
+
+        createChart(`graph_dist_class`, `bar`, filteredDataDist.class);
+        createChart(`graph_dist_class_1`, `bar`, filteredDataDist.class_1);
+        createChart(`graph_dist_class_1_13`, `bar`, filteredDataDist.class_1_13);
+        createChart(`graph_dist_class_1_14`, `bar`, filteredDataDist.class_1_14);
+        createChart(`graph_dist_class_1_15`, `bar`, filteredDataDist.class_1_15);
+        createChart(`graph_dist_class_12`, `bar`, filteredDataDist.class_12);
+
+
+        for (let i = 0; i < upazilaName.length; i++) {
+            upazilaName[i].textContent = ''
+            upazilaName[i].textContent = uniquefilteredDataDiv[2]; // You can perform actions on each element here
+        }
+        let filteredDataUpe = generateChartData(filteredFeaturesUpe)
+        createChart(`graph_upazila_class`, `bar`, filteredDataUpe.class);
+        createChart(`graph_upazila_class_1`, `bar`, filteredDataUpe.class_1);
+        createChart(`graph_upazila_class_1_13`, `bar`, filteredDataUpe.class_1_13);
+        createChart(`graph_upazila_class_1_14`, `bar`, filteredDataUpe.class_1_14);
+        createChart(`graph_upazila_class_1_15`, `bar`, filteredDataUpe.class_1_15);
+        createChart(`graph_upazila_class_12`, `bar`, filteredDataUpe.class_12);
+
+    }
+   
+
+    layer.changed();
     } else {
         console.log('No feature filtered')
     }
@@ -314,27 +489,8 @@ function onclickDivision(term) {
     /*****************************************************************************/
 
 
-    /**featureKeys.forEach(className => className)
-    // Iterate over the array using forEach
-
-    const filteredData = generateChartData(filteredFeatures);
-
-    // Create charts
-    clearDivCharts()
-    clearDistCharts()
-    clearUpazilaCharts()
-
-
-
-
-    createChart(`graph_div_class`, `bar`, filteredData.class);
-    createChart(`graph_div_class_1`, `bar`, filteredData.class_1);
-    createChart(`graph_div_class_1_13`, `bar`, filteredData.class_1_13);
-    createChart(`graph_div_class_1_14`, `bar`, filteredData.class_1_14);
-    createChart(`graph_div_class_1_15`, `bar`, filteredData.class_1_15);
-    createChart(`graph_div_class_12`, `bar`, filteredData.class_12);***/
-
-    layer.changed();
+    
+   
 }
 
 
